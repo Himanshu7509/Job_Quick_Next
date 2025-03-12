174c387a69fc1a38b8bf744b7b471c889b469832
HIMANSHU DIWAKAR THAKRE <himanshuthakre7509@gmail.com>
2025-03-12 13:52:44 +0530

    4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 
-import SeekerSignup from '@/components/auth/seeker/SeekerSignup'
+
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <SeekerSignup/>Ho
+   Home
   
     </div>
   )
